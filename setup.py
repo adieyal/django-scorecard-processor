7cffd14303dec94fef8454b06fa0febf1e0b898d
Bradley Whittington <radbrad182@gmail.com>
2011-10-24 13:03:31 +0200

    Add stub files for the app

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+import platform
+
+DESCRIPTION = "A Django reusable application for collecting data and transforming it into scorecards"
+
+LONG_DESCRIPTION = None
+try:
+    LONG_DESCRIPTION = open('README').read()
+except:
+    pass
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Framework :: Django',
+]
+
+setup(
+    name='django-scorecard-processor',
+    version='0.1',
+    packages=['scorecard_processor'],
+    author='Bradley Whittington',
+    author_email='radbrad182@gmail.com',
+    url='http://github.com/bradwhittington/django-scorecard-processor/',
+    license='MIT',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    platforms=['any'],
+    classifiers=CLASSIFIERS,
+)
+
