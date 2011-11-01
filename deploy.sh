ad68dc6611252eef2b6e3634872feaf90d28ce77
Bradley Whittington <radbrad182@gmail.com>
2011-11-01 11:02:41 +0200

    Renaming some things

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd `dirname $0`
 rm -rf /tmp/example_app/
-cp -RL example_app /tmp/
+cp -R example_app /tmp/
+cp -R django-scorecard-processor/scorecard_processor /tmp/example_app/
 cd /tmp/example_app/
 epio upload
 rm -rf /tmp/example_app/
