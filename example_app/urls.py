9051fedc5eb70cbee4c871c3f205ff246c186170
Bradley Whittington <radbrad182@gmail.com>
2011-11-01 06:42:29 +0200

    Masks out models code in scorecard for now. Gets base templates and app working nice nice

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template, redirect_to
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
     # (r'^example_app/', include('example_app.foo.urls')),
+    (r'^$', direct_to_template, {'template':'index.html'}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
