4fd8b641a41bc7e541bb455d8196970965e29a9f
Bradley Whittington <radbrad182@gmail.com>
2011-11-02 17:35:02 +0200

    Set appropriate redirect url

--- example_app/settings.py
+++ example_app/settings.py
@@ -20,6 +20,8 @@ DATABASES = {
     }
 }
 
+LOGIN_REDIRECT_URL = "/"
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
