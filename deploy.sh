1e9e7450fc3701fae86b9bbfe05268d25915d109
Bradley Whittington <radbrad182@gmail.com>
2011-11-02 13:30:47 +0200

    Manage the hot symlinks in deploy

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 cd `dirname $0`
 rm -rf /tmp/example_app/
 cp -R example_app /tmp/
+rm /tmp/example_app/scorecard_processor
 cp -R django-scorecard-processor/scorecard_processor /tmp/example_app/
 cd /tmp/example_app/
 epio upload
