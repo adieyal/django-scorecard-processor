20cdfbbe8f3b80bc187cf5397706fb5743e9cb20
Bradley Whittington <radbrad182@gmail.com>
2011-11-03 10:48:14 +0200

    Add models to admin

---
+++ django-scorecard-processor/scorecard_processor/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from models import  DataSeries, Entity, Survey, Question, ResponseSet, Response, Scorecard, Operation, OperationArgument
+
+admin.site.register(DataSeries)
+admin.site.register(Entity)
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(ResponseSet)
+admin.site.register(Response)
+admin.site.register(Scorecard)
+admin.site.register(Operation)
+admin.site.register(OperationArgument)
