b76eca6bf25eeb7abd02688c1c198ed4765a058a
Bradley Whittington <radbrad182@gmail.com>
2011-11-09 15:15:20 +0200

    Force logins for certain views

--- django-scorecard-processor/scorecard_processor/urls.py
+++ django-scorecard-processor/scorecard_processor/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls.defaults import *
 from django.views.generic.list_detail import object_list, object_detail
+from django.contrib.auth.decorators import login_required
+
 from models import Entity
 
 urlpatterns = patterns('scorecard_processor.views',
     (r'^$', 'index'),
     url(r'^entity/$', #TODO:limit entities to the ones a user account can access
-        object_list, 
+        login_required(object_list), 
         {'queryset':Entity.objects.all()},
         name="entity_list"
     ), 
     url(r'^entity/(?P<object_id>\d+)/$', 
-        object_detail,
+        login_required(object_detail),
         {'queryset':Entity.objects.all()}, 
         name="show_entity"
     ),
--- django-scorecard-processor/scorecard_processor/views.py
+++ django-scorecard-processor/scorecard_processor/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from models import ResponseSet, Survey, Entity
 from forms import QuestionForm
@@ -8,6 +9,7 @@ from forms import QuestionForm
 def index(request):
     return render_to_response('scorecard_processor/index.html',{},RequestContext(request))
 
+@login_required
 def show_survey(request, object_id, survey_id):
     survey = get_object_or_404(Survey, pk=survey_id)
     entity = get_object_or_404(Entity, pk=object_id)
