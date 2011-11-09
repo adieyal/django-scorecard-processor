7e81fe98c96b258bacca025c5f2b3f5e52f9cb17
Bradley Whittington <radbrad182@gmail.com>
2011-11-09 13:12:14 +0200

    Tweaks to make deploy and reset scripts even more awesome

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ rm /tmp/example_app/scorecard_processor
 cp -R django-scorecard-processor/scorecard_processor /tmp/example_app/
 cd /tmp/example_app/
 epio upload
-epio django syncdb
+epio django syncdb -- --noinput
 epio django migrate
 cd -
 rm -rf /tmp/example_app/
--- reset.sh
+++ reset.sh
@@ -3,5 +3,9 @@ cd `dirname $0`
 cd example_app
 epio delete
 rm .epio-app
+sleep 5
 epio create ihpscorecard
 ../deploy.sh
+epio django loaddata example_data.json
+epio django createsuperuser -- --username=admin --email=admin@example.org --noinput
+echo -e "from django.contrib.auth import models;models.User.objects.get(username='admin').set_password('abc123');exit()" | epio django shell
