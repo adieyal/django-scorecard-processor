8a37065e053370bff6d703bdaf9baac1fa423e7d
Bradley Whittington <radbrad182@gmail.com>
2011-11-25 15:04:46 +0200

    Adds a mechanism for importing IHP data

--- local_reset.sh
+++ local_reset.sh
@@ -5,4 +5,4 @@ rm testing.db;
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | ./manage.py shell --settings=local_settings 
 ./manage.py loaddata example_data.json --settings=local_settings 
 ./manage.py loaddata ../local_session.json --settings=local_settings 
-echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | ./manage.py shell --settings=local_settings
+#echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | ./manage.py shell --settings=local_settings
--- reset.sh
+++ reset.sh
@@ -9,4 +9,4 @@ epio create ihpscorecard
 epio django loaddata example_data.json
 epio django createsuperuser -- --username=admin --email=admin@example.org --noinput
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | epio django shell
-echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | epio django shell
+#echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | epio django shell
