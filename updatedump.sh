c7aa270fc5ada00ac7e8074afcbdd002f3110490
Bradley Whittington <radbrad182@gmail.com>
2011-12-13 10:53:03 +0200

    Run update s ript

--- updatedump.sh
+++ updatedump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e "from scorecard_processor import models;models.ResponseSet.objects.all().delete();models.CachedResult.objects.all.delete();exit()" | ./manage.py shell --settings=local_settings 
+echo -e "from scorecard_processor import models;models.ResponseSet.objects.all().delete();models.CachedResult.objects.all().delete();exit()" | ./manage.py shell --settings=local_settings 
 ./manage.py dumpdata scorecard_processor --settings=local_settings --indent=2 > example_data.json
