fb86ca30d4a9f570d2163c3d4d31356df6c6a751
Bradley Whittington <radbrad182@gmail.com>
2011-12-16 23:48:28 +0200

    Tweaks

--- example_app/local_settings.py
+++ example_app/local_settings.py
@@ -21,12 +21,12 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 DEBUG_TOOLBAR_PANELS = (
-    #'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.version.VersionDebugPanel',
     'debug_toolbar.panels.timer.TimerDebugPanel',
-    #'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
     'debug_toolbar.panels.headers.HeaderDebugPanel',
     'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    #'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
     'debug_toolbar.panels.sql.SQLDebugPanel',
     'debug_toolbar.panels.signals.SignalDebugPanel',
     'debug_toolbar.panels.logger.LoggingPanel',
@@ -34,8 +34,8 @@ DEBUG_TOOLBAR_PANELS = (
 
 CACHES = {
     'default': {
-            'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-            #'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-            #'LOCATION': 'unique-snowflake'
+            #'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+            'LOCATION': 'unique-snowflake'
     }
 }
