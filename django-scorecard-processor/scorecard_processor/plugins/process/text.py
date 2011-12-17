f937e6b60aff0cbb4c2d851aa7c8180191965243
Bradley Whittington <radbrad182@gmail.com>
2011-12-17 11:49:29 +0200

    Render text concatenation better

--- django-scorecard-processor/scorecard_processor/plugins/process/text.py
+++ django-scorecard-processor/scorecard_processor/plugins/process/text.py
@@ -3,14 +3,26 @@ from scorecard_processor.plugins import base, register
 def sum_values(x, y):
     return x + y
 
+class TextRenderer(base.Scalar):
+    def get_value(self):
+        "\n\n".join([i[1] for i in self.value])
+
+    def _as_html(self):
+        if not self.value:
+            return ''
+        row = "<dt>%s</dt><dd>%s</dd>"
+        return "<dl class='concat'>%s</dl>" % ''.join([row % i for i in self.value])
+
+        
+
 class Concat(base.ProcessPlugin):
     name = 'Concatenate responses'
     argument_list = ['response']
     input_type = base.Vector
-    output_type = base.Scalar
+    output_type = TextRenderer
     allow_cache = False
 
     def process(self):
-        return self.output_type("\n\n".join([v.get_value() for v in self.get_arguments().response.get_values()]))
+        return self.output_type([(v.response_set.entity, v.get_value()) for v in self.get_arguments().response.get_values()])
 
 register.register('process','Text','concat',Concat)
