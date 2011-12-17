5a0d005f2f76898ca71282ff6c7dfb756eb42d84
Bradley Whittington <radbrad182@gmail.com>
2011-12-17 11:16:32 +0200

    Makes variables better at rendering themselves

--- django-scorecard-processor/scorecard_processor/plugins/types.py
+++ django-scorecard-processor/scorecard_processor/plugins/types.py
@@ -1,3 +1,5 @@
+from django.utils.safestring import mark_safe
+
 class Value(object):
     def __init__(self, item, extractor_class=None):
         if extractor_class:
@@ -24,6 +26,12 @@ class Scalar(Value):
         except AttributeError:
             return self.value
 
+    def _as_html(self):
+        return self.value
+
+    def as_html(self):
+        return mark_safe(self._as_html())
+
 class Vector(Value):
     def __init__(self, item, extractor_class=None):
         if extractor_class:
@@ -42,3 +50,9 @@ class Vector(Value):
             return self.value.get_values()
         except AttributeError:
             return self.value
+
+    def _as_html(self):
+        return "<ul>%s</ul>" % ''.join(["<li>%s</li>" % v for v in self.value])
+
+    def as_html(self):
+        return mark_safe(self._as_html())
