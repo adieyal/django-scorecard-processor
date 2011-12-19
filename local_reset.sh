149b1535f7fcf36c2a13a71cda1615902f35caf1
Bradley Whittington <radbrad182@gmail.com>
2011-12-19 09:14:53 +0200

    Fix for issue #2

--- local_reset.sh
+++ local_reset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rm testing.db; 
+rm -f testing.db; 
 ./manage.py syncdb --noinput --settings=local_settings
 ./manage.py createsuperuser --username=admin --email=admin@example.org --noinput --settings=local_settings 
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | ./manage.py shell --settings=local_settings 
