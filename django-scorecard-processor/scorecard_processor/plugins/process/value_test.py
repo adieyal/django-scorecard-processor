5ae8e815f628040e8c4c077c2fd0b09ae0e218c2
Bradley Whittington <radbrad182@gmail.com>
2011-12-19 10:42:58 +0200

    Adds combination

--- django-scorecard-processor/scorecard_processor/plugins/process/value_test.py
+++ django-scorecard-processor/scorecard_processor/plugins/process/value_test.py
@@ -16,3 +16,26 @@ class SingleValue(base.ProcessPlugin):
         return self.output_type('n')
 
 register.register('process','Assert value','assert_value',SingleValue)
+
+class CombineValues(base.ProcessPlugin):
+    name = 'Combine Values'
+    argument_list = ['items']
+    input_type = base.Vector
+    output_type = base.Scalar
+    options = { 'value': basestring }
+    defaults = { 'value':'yes' }
+
+    def process(self):
+        values = self.get_arguments().items.get_values()
+        output = []
+        for value in values:
+            
+            if value.get_value():
+                if value.get_value().lower() == self.get_config('value'):
+                    return output.append('y')
+                else:
+                    return output.append('y')
+            else:
+                output.append(' ')
+        return self.output_type(''.join(output))
+register.register('process','Combine values','combine_values',CombineValues)
