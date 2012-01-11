23f1c0e88849a804e4f57e66785793dcacdb249c
Bradley Whittington <radbrad182@gmail.com>
2012-01-11 15:06:57 +0200

    Shuffle resource files into the right places for ihp

--- reset.sh
+++ reset.sh
@@ -6,7 +6,7 @@ rm .epio-app
 sleep 5
 epio create ihpscorecard
 ../deploy.sh
-epio django loaddata example_data.json
+epio django loaddata ihp_results/example_data.json
 epio django createsuperuser -- --username=admin --email=admin@example.org --noinput
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | epio django shell
 #echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | epio django shell
--- updatedump.sh
+++ updatedump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -e "from scorecard_processor import models;models.ResponseSet.objects.all().delete();exit()" | ./manage.py shell --settings=local_settings 
-./manage.py dumpdata scorecard_processor --settings=local_settings --indent=2 > example_data.json
+./manage.py dumpdata scorecard_processor --settings=local_settings --indent=2 > ihp_results/example_data.json
