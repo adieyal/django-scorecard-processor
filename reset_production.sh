a350cf15e6526f5df2486d70bb604ad203d5c24b
Bradley Whittington <radbrad182@gmail.com>
2012-02-04 19:00:25 +0200

    Add users with limited permissions

--- reset_production.sh
+++ reset_production.sh
@@ -7,4 +7,6 @@ echo -e "from django.contrib.auth import models;u=models.User.objects.get(userna
 echo -e "from django.contrib.sites import models;s=models.Site.objects.get_current();s.name='IHP+Results';s.domain='survey2012.ihpresults.net';s.save();exit()" | epio django shell -a ihp_prod
 ../deploy_ihp.sh
 epio django loaddata ihp_results/example_data.json -a ihp_prod
+echo -e "from django.contrib.auth import models;from scorecard_processor.models import Entity;u=models.User.objects.create(username='radbrad182@gmail.com', email='radbrad182@gmail.com', first_name='Bradley', last_name='Whittington');u.set_password('abc123');u.entity_set.add(Entity.objects.get(abbreviation='WHO'));u.save();exit()" | ./manage.py shell --settings=local_settings 
+echo -e "from django.contrib.auth import models;from scorecard_processor.models import Entity;u=models.User.objects.create(username='james@human-scale.net', email='james@human-scale.net', first_name='James', last_name='Fairfax');u.set_password('abc123');u.entity_set.add(Entity.objects.get(abbreviation='WHO'));u.save();exit()" | ./manage.py shell --settings=local_settings 
 #echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | epio django shell
