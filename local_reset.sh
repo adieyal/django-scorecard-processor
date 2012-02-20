32b658e88fe373330645553cfa016ea4a8cb7142
Bradley Whittington <radbrad182@gmail.com>
2012-02-20 16:52:31 +0200

    More french things

--- local_reset.sh
+++ local_reset.sh
@@ -4,6 +4,9 @@
 ./manage.py syncdb --noinput --settings=local_settings
 ./manage.py createsuperuser --username=admin --email=admin@example.org --noinput --settings=local_settings 
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | ./manage.py shell --settings=local_settings 
-./manage.py loaddata ihp_results/example_data.json --settings=local_settings 
+./manage.py loaddata ihp_results/project_data/project.json   --settings=local_settings 
+./manage.py loaddata ihp_results/project_data/entities.json   --settings=local_settings 
+./manage.py loaddata ihp_results/project_data/surveys.json --settings=local_settings 
+./manage.py loaddata ihp_results/project_data/reports.json   --settings=local_settings 
 echo -e "from django.contrib.auth import models;from scorecard_processor.models import Entity;u=models.User.objects.create(username='radbrad182@gmail.com', email='radbrad182@gmail.com', first_name='Bradley', last_name='Whittington');u.set_password('abc123');u.entity_set.add(Entity.objects.get(abbreviation='WHO'));u.save();exit()" | ./manage.py shell --settings=local_settings 
 #echo -e "from scorecard_processor.models import ReportRun;r=ReportRun.objects.get(id=1);r.source_data={'data_series':5};r.save();exit()" | ./manage.py shell --settings=local_settings
