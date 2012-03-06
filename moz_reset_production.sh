825a95b4503d03c587caf369162277ea9f1519e0
Bradley Whittington <radbrad182@gmail.com>
2012-03-06 15:56:24 +0200

    Fix the branding

--- moz_reset_production.sh
+++ moz_reset_production.sh
@@ -5,7 +5,7 @@ epio django flush  -- --noinput
 epio django reset scorecard_processor -- --noinput
 epio django createsuperuser  -- --username=admin --email=admin@example.org --noinput 
 echo -e "from django.contrib.auth import models;u=models.User.objects.get(username='admin');u.set_password('abc123');u.save();exit()" | epio django shell 
-echo -e "from django.contrib.sites import models;s=models.Site.objects.get_current();s.name='IHP+Results';s.domain='survey2012.mozresults.net';s.save();exit()" | epio django shell 
+echo -e "from django.contrib.sites import models;s=models.Site.objects.get_current();s.name='Mozambique';s.domain='moz2012.ihpresults.net';s.save();exit()" | epio django shell 
 ../deploy_moz.sh
 epio django loaddata moz_results/project_data/project.json
 epio django loaddata moz_results/project_data/entities.json
