07352e688bc5e1b37eb33e50c6cb4070105a8687
Baterikov Anton <baterikov@gmail.com>
2012-05-28 11:52:40 +0300

    Added ierg import script

---
+++ ierg/local_settings.py
@@ -0,0 +1,59 @@
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'testing.db',                      # Or path to database file if using
+    }
+}
+
+STATICFILES_DIRS = (
+    'static/',
+)
+
+#INSTALLED_APPS  = INSTALLED_APPS+('debug_toolbar',)
+#MIDDLEWARE_CLASSES = ('debug_toolbar.middleware.DebugToolbarMiddleware',) + MIDDLEWARE_CLASSES
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_CONFIG = {
+   "INTERCEPT_REDIRECTS":False,
+   "SQL_DUPLICATES":True,
+#    "ENABLE_STACKTRACES":False,
+}
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+CACHES = {
+    'default': {
+            #'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+            'LOCATION': 'unique-snowflake',
+    }
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'survey2012@ihpresults.net'
+EMAIL_HOST_PASSWORD = 'surveytools'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+SERVER_EMAIL='"IHP+Results Survey 2012" <survey2012@ihpresults.net>'
+DEFAULT_FROM_EMAIL=SERVER_EMAIL
+ADMINS = (
+    ('Sysadmin','sysadmin@ihpresults.net'),
+)
+EMAIL_SUBJECT_PREFIX='[Local] '
+
+    
---
+++ ierg/urls.py
@@ -0,0 +1,27 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+from django.views.generic.simple import direct_to_template
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    (r'^$', direct_to_template, {'template':'index.html'}),
+    (r'^accounts/', include('django.contrib.auth.urls')),
+    (r'^scorecard/', include('scorecard_processor.urls')),
+
+    (r'^i18n/', include('django.conf.urls.i18n')),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    (r'^admin/', include(admin.site.urls)),
+    (r'^grappelli/', include('grappelli.urls')),
+
+    (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root': 'static'}),
+
+)
+
+
