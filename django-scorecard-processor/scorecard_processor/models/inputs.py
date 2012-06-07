395b9bf840d83273c15b1fcb3ae3ba0c090959bb
Andrey Makhonin <makhonin@steelkiwi.com>
2012-06-07 22:51:26 +0300

    Models for excelfile bugfix

--- django-scorecard-processor/scorecard_processor/models/inputs.py
+++ django-scorecard-processor/scorecard_processor/models/inputs.py
@@ -403,24 +403,42 @@ class ExcelFile(models.Model):
         new = False if self.id else True
         super(ExcelFile, self).save(*args, **kwargs)
         if new:
-            call_command('1_1_birth_registration', self.excel_file.path, survey_name='BirthRegistration', excel_file_id=self.id)
-            call_command('1_2_death_registration', self.excel_file.path, survey_name='DeathRegistration', excel_file_id=self.id)
-            call_command('1_3_maternal_death_reviews', self.excel_file.path, survey_name='MaternalDeathReviews', excel_file_id=self.id)
-            call_command('1_4_cod', self.excel_file.path, survey_name='COD', excel_file_id=self.id)
-            call_command('1_5_qoc', self.excel_file.path, survey_name='QoC', excel_file_id=self.id)
-            call_command('1_7_crvs_improvement', self.excel_file.path, survey_name='CRVSImprovement', excel_file_id=self.id)
-            call_command('2_1_health_info', self.excel_file.path, survey_name='HealthInfo', excel_file_id=self.id)
-            call_command('2_2_coverage_indicators', self.excel_file.path, survey_name='CoverageIndicators', excel_file_id=self.id)
-            call_command('2_3_impact_indicators', self.excel_file.path, survey_name='ImpactIndicators', excel_file_id=self.id)
-            call_command('3_1_ehealth_strategy', self.excel_file.path, survey_name='EhealthStrategy', excel_file_id=self.id)
-            call_command('3_2_web_based_reporting', self.excel_file.path, survey_name='WebBasedReporting', excel_file_id=self.id)
-            call_command('4_1_total_health_expenditure', self.excel_file.path, survey_name='TotalHealthExpenditure', excel_file_id=self.id)
-            call_command('4_2_nha_conducted', self.excel_file.path, survey_name='NhaConducted', excel_file_id=self.id)
-            call_command('4_3_rmnch_expenditure', self.excel_file.path, survey_name='RmnchExpenditure', excel_file_id=self.id)
-            call_command('5_1_financial_reporting_system', self.excel_file.path, survey_name='FinancialReportingSystem', excel_file_id=self.id)
-            call_command('6_1_reviews_of_health_spending', self.excel_file.path, survey_name='ReviewsOfHealthSpending', excel_file_id=self.id)
-            call_command('7_1_reviews_of_performance', self.excel_file.path, survey_name='ReviewsOfPerformance', excel_file_id=self.id)
-            call_command('8_1_performance_report_public', self.excel_file.path, survey_name='PerformanceReportPublic', excel_file_id=self.id)
+            call_command('1_1_birth_registration', file_path=self.excel_file.path,
+                survey_name='BirthRegistration', excel_file_id=self.id)
+            call_command('1_2_death_registration', file_path=self.excel_file.path,
+                survey_name='DeathRegistration', excel_file_id=self.id)
+            call_command('1_3_maternal_death_reviews', file_path=self.excel_file.path,
+                survey_name='MaternalDeathReviews', excel_file_id=self.id)
+            call_command('1_4_cod', file_path=self.excel_file.path,
+                survey_name='COD', excel_file_id=self.id)
+            call_command('1_5_qoc', file_path=self.excel_file.path,
+                survey_name='QoC', excel_file_id=self.id)
+            call_command('1_7_crvs_improvement', file_path=self.excel_file.path,
+                survey_name='CRVSImprovement', excel_file_id=self.id)
+            call_command('2_1_health_info', file_path=self.excel_file.path,
+                survey_name='HealthInfo', excel_file_id=self.id)
+            call_command('2_2_coverage_indicators', file_path=self.excel_file.path,
+                survey_name='CoverageIndicators', excel_file_id=self.id)
+            call_command('2_3_impact_indicators', file_path=self.excel_file.path,
+                survey_name='ImpactIndicators', excel_file_id=self.id)
+            call_command('3_1_ehealth_strategy', file_path=self.excel_file.path,
+                survey_name='EhealthStrategy', excel_file_id=self.id)
+            call_command('3_2_web_based_reporting', file_path=self.excel_file.path,
+                survey_name='WebBasedReporting', excel_file_id=self.id)
+            call_command('4_1_total_health_expenditure', file_path=self.excel_file.path,
+                survey_name='TotalHealthExpenditure', excel_file_id=self.id)
+            call_command('4_2_nha_conducted', file_path=self.excel_file.path,
+                survey_name='NhaConducted', excel_file_id=self.id)
+            call_command('4_3_rmnch_expenditure', file_path=self.excel_file.path,
+                survey_name='RmnchExpenditure', excel_file_id=self.id)
+            call_command('5_1_financial_reporting_system', file_path=self.excel_file.path,
+                survey_name='FinancialReportingSystem', excel_file_id=self.id)
+            call_command('6_1_reviews_of_health_spending', file_path=self.excel_file.path,
+                survey_name='ReviewsOfHealthSpending', excel_file_id=self.id)
+            call_command('7_1_reviews_of_performance', file_path=self.excel_file.path,
+                survey_name='ReviewsOfPerformance', excel_file_id=self.id)
+            call_command('8_1_performance_report_public', file_path=self.excel_file.path,
+                survey_name='PerformanceReportPublic', excel_file_id=self.id)
 
 
 def invalidate_old_responses(sender, instance, **kwargs):
--- ierg/ierg_results/management/commands/ierg_command.py
+++ ierg/ierg_results/management/commands/ierg_command.py
@@ -10,6 +10,9 @@ class IergCommand(BaseCommand):
     args = '<filename.xlsx>'
     help = 'Imports xls file into system'
     option_list = BaseCommand.option_list + (
+        make_option('--file_path',
+            dest='file_path',
+            help='Give the file path'),
         make_option('--survey_name',
             dest='survey_name',
             help='Give the survey a name'),
@@ -25,61 +28,60 @@ class IergCommand(BaseCommand):
 
 
     def handle(self, *args, **options):
-        excel_file_id = options.get('excel_file_id')
-        excel_file = models.ExcelFile.objects.get(id=excel_file_id)
+        try:
+            file_path = options.get('file_path')
+            survey_name = options.get('survey_name')
+            excel_file_id = options.get('excel_file_id')
 
-        wb = load_workbook(filename=args[0])
+            excel_file = models.ExcelFile.objects.get(id=excel_file_id)
+            wb = load_workbook(filename=file_path)
+            sheet = wb.get_sheet_by_name(name=self.SHEET_NAME)
 
-        survey_name = options.get('survey_name')
+            column_names = {k:v.value for k, v in\
+                enumerate(sheet.range(self.COLUM_NAME_ROW_STRING)[0])}
 
-        sheet = wb.get_sheet_by_name(name=self.SHEET_NAME)
+            ds_group = models.DataSeriesGroup.objects.get(name=column_names[0])
+            entity_type = models.EntityType.objects.get(name=column_names[0])
+            del column_names[0]
 
-        column_names = {k:v.value for k, v in\
-            enumerate(sheet.range(self.COLUM_NAME_ROW_STRING)[0])}
+            survey, created = models.Survey.objects.get_or_create(
+                name=survey_name, project=self.PROJECT)
 
-        ds_group = models.DataSeriesGroup.objects.get(name=column_names[0])
-        entity_type = models.EntityType.objects.get(name=column_names[0])
-        del column_names[0]
+            if created:
+                survey.data_series_groups.add(ds_group)
+                survey.entity_types.add(entity_type)
 
+            survey.question_set.all().delete()
+            survey.questiongroup_set.all().delete()
 
-        survey, created = models.Survey.objects.get_or_create(
-            name=survey_name, project=self.PROJECT)
-
-        if created:
-            survey.data_series_groups.add(ds_group)
-            survey.entity_types.add(entity_type)
-
-        survey.question_set.all().delete()
-        survey.questiongroup_set.all().delete()
-
-        question = models.Question.objects.create(survey=survey, group=None,
-            identifier=self.SHEET_NAME, question=self.SHEET_NAME)
+            question = models.Question.objects.create(survey=survey, group=None,
+                identifier=self.SHEET_NAME, question=self.SHEET_NAME)
 
 
-        for i in xrange(self.START_LINE, self.FINISH_LINE):
-            cell = sheet.cell(row=i, column=0)
+            for i in xrange(self.START_LINE, self.FINISH_LINE):
+                cell = sheet.cell(row=i, column=0)
 
-            entity, created = models.Entity.objects.get_or_create(
-                name=cell.value, entity_type=entity_type, project=self.PROJECT)
+                entity, created = models.Entity.objects.get_or_create(
+                    name=cell.value, entity_type=entity_type, project=self.PROJECT)
 
-            ds, created = models.DataSeries.objects.get_or_create(
-                name=cell.value, group=ds_group)
+                ds, created = models.DataSeries.objects.get_or_create(
+                    name=cell.value, group=ds_group)
 
-            response_set, created = models.ResponseSet.objects.get_or_create(
-                survey=survey, entity=entity)
-            response_set.data_series.add(ds)
+                response_set, created = models.ResponseSet.objects.get_or_create(
+                    survey=survey, entity=entity)
+                response_set.data_series.add(ds)
 
-            try:
                 json = self.get_json(sheet, column_names, i)
-            except TypeError, exc:
-                excel_file.parse_log += '%s\n' % exc
 
-            response = models.Response.objects.create(question=question,
-                response_set=response_set, respondant=self.USER, value=json)
+                response = models.Response.objects.create(question=question,
+                    response_set=response_set, respondant=self.USER, value=json)
 
 
-        excel_file.parse_log += 'Done\n'
-        excel_file.save()
+            excel_file.parse_log += '%s - parse completed\n' % self.SHEET_NAME
+        except Exception, exc:
+            excel_file.parse_log += '%s - %s\n' % (self.SHEET_NAME, exc)
+        finally:
+            excel_file.save()
 
 
     def get_json(self, sheet, column_names, i):
